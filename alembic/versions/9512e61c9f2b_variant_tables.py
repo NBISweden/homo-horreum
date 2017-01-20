6051462cf2b17e673a17674988a2292dd6063343
Johan Viklund <johan.viklund@nbis.se>
2017-01-20 14:45:00 +0100

    Add the variant tables (for snps and so forth)

---
+++ alembic/versions/9512e61c9f2b_variant_tables.py
@@ -0,0 +1,51 @@
+"""Variant tables
+
+Revision ID: 9512e61c9f2b
+Revises: 52337e31da34
+Create Date: 2017-01-20 10:45:52.627849
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9512e61c9f2b'
+down_revision = '52337e31da34'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('variant',
+            sa.Column('id', sa.Integer, primary_key=True),
+            sa.Column('chromosome', sa.String(2), nullable=False),
+            sa.Column('pos', sa.Integer, nullable=False),
+            sa.Column('identifier', sa.String(), nullable=False),
+            sa.Column('ref', sa.String(), nullable=False),
+            sa.Column('alt', sa.String(), nullable=False)
+    )
+
+    op.create_table('variant_extra_fields',
+            sa.Column('id', sa.Integer, primary_key=True),
+            sa.Column('name', sa.Text, nullable=False)
+    )
+
+    op.create_table('variant_extra',
+            sa.Column('variant_id', sa.Integer, sa.ForeignKey('variant.id')),
+            sa.Column('field_id', sa.Integer, sa.ForeignKey('variant_extra_fields.id')),
+            sa.Column('value', sa.Text, nullable=False)
+    )
+
+    op.create_table('person_variant',
+            sa.Column('person_id', sa.Integer, sa.ForeignKey('person.id')),
+            sa.Column('variant_id', sa.Integer, sa.ForeignKey('variant.id'))
+    )
+
+
+
+def downgrade():
+    op.drop_table('variant')
+    op.drop_table('variant_extra_fields')
+    op.drop_table('variant_extra')
+    op.drop_table('person_variant')
