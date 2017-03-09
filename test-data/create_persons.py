e9cc807c99e7e1cf4885b96a35f92b16adc6ac09
Johan Viklund <johan.viklund@nbis.se>
2017-03-09 15:04:51 +0100

    Naming fixes (patient->person)

--- test-data/create_persons.py
+++ test-data/create_persons.py
@@ -1,7 +1,7 @@
 import random
 import sqlalchemy as sa
 
-def insert_patients(num=100):
+def insert_persons(num=100):
     engine = sa.create_engine('sqlite:///test.db', echo=False)
     metadata = sa.MetaData()
     person = sa.Table('person', metadata, autoload=True, autoload_with=engine)
@@ -10,14 +10,14 @@ def insert_patients(num=100):
     trans = conn.begin()
 
     for i in range(num):
-        patient_identifier = "pt{:03}".format(i)
-        print("Inserting {}".format(patient_identifier))
+        person_identifier = "pt{:03}".format(i)
+        print("Inserting {}".format(person_identifier))
         conn.execute(insert,
-                identifier=patient_identifier,
+                identifier=person_identifier,
                 group = random.choice(['control', 'exp']),
                 sex   = random.choice(['M','F'])
                 )
     trans.commit()
 
 if __name__ == '__main__':
-    insert_patients(200)
+    insert_persons(200)
