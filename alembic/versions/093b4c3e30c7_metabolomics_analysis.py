b2a769993bd325bfb29d830ca03edde75cbc9580
Simon Ekström <simon.ekstrom@gmail.com>
2017-11-21 13:23:36 +0100

    Initialized new migration

---
+++ alembic/versions/093b4c3e30c7_metabolomics_analysis.py
@@ -0,0 +1,24 @@
+"""metabolomics_analysis
+
+Revision ID: 093b4c3e30c7
+Revises: 332835b86836
+Create Date: 2017-11-21 13:20:53.415880
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '093b4c3e30c7'
+down_revision = '332835b86836'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
